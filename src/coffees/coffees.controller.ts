7e0559c7fbd006d72b6a249c595bf076476fec8c
rmasonhx12 <rmasonh22@gmail.com>
2023-02-26 18:19:44 -0500

    remove commented out code --clean up

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -21,34 +21,25 @@ export class CoffeesController {
   @Get()
   findAll(@Query() paginationQuery) {
     return this.coffeesService.findAll();
-    //const { limit, offset } = paginationQuery;
-    //return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
   }
 
   // add id parameter --
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.coffeesService.findOne(id);
-    //return `This action returns #${id} coffee`;
   }
 
   // body decorator
   @Post()
-  //@HttpCode(HttpStatus.GONE)
-  //create (@Body('name') body) {  --> pulls name out  ==> susceptible to vslidation errors USE WITH CAUTION
   create(@Body() body) {
     return this.coffeesService.create(body);
-    //return body;
-    //return `This action creates a coffee`;
   }
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
     return this.coffeesService.update(id, body);
-    //return `This action updates #${id} coffee`;
   }
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.coffeesService.remove(id);
-    //return `This action removes #${id} coffee`;
   }
 }
