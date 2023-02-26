79afc16e9e45b32860e7f55df483468eac9b8868
rmasonhx12 <rmasonh22@gmail.com>
2023-02-26 10:10:51 -0500

    post

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -11,5 +11,13 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns #${id} coffee`;
+  }
+
+  // body decorator
+  @Post()
+  //create (@Body('name') body) {  --> pulls name out  ==> susceptible to vslidation errors USE WITH CAUTION
+  create(@Body() body) {
+    return body;
+    //return `This action creates a coffee`;
   }
 }
