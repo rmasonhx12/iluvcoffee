b9cdeac2e11ca866cdc4751cac72a8468bb63ed3
rmasonhx12 <rmasonh22@gmail.com>
2023-02-26 10:45:31 -0500

    native --response --use with care as lose comp with nest

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
-  findAll() {
-    return 'This action returns all coffees';
+  @Get()
+  findAll(@Res() response) {
+    response.status(200).send('This action returns all coffees');
   }
 
   // add id parameter --
@@ -15,6 +24,7 @@ export class CoffeesController {
 
   // body decorator
   @Post()
+  @HttpCode(HttpStatus.GONE)
   //create (@Body('name') body) {  --> pulls name out  ==> susceptible to vslidation errors USE WITH CAUTION
   create(@Body() body) {
     return body;
