5453902c993f86047315d3380ddefab9439dd3e4
rmasonhx12 <rmasonh22@gmail.com>
2023-02-26 09:57:51 -0500

    add dynamic id to coffees

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
   findAll() {
     return 'This action returns all coffees';
+  }
+
+  // add id parameter --
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
   }
 }
